def8f170c89fd98e454d0d5e73ecf52e9a4bec8f
jtorreggiani <joetorreggiani@gmail.com>
2024-11-27 00:44:05 -0500

    Add pytest dependency to bootstrap script.

--- src/create_mcp_server/__init__.py
+++ src/create_mcp_server/__init__.py
@@ -182,6 +182,7 @@ def create_project(
     # Add mcp dependency using uv add
     try:
         subprocess.run(["uv", "add", "mcp"], cwd=path, check=True)
+        subprocess.run(["uv", "add", "pytest", '--dev'], cwd=path, check=True)
     except subprocess.CalledProcessError:
         click.echo("❌ Error: Failed to add mcp dependency.", err=True)
         sys.exit(1)
